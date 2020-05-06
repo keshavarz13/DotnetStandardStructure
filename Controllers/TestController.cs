67faa00a7caca09bd40ea9f856ada74d3b1450d3
Mohammad Ali Keshavarz <ma.keshavarz@Alibaba.ir>
2020-05-06 16:50:19 +0430

    Init project and configure swagger

---
+++ Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("api/user")]
+        public IActionResult getUser()
+        {
+            return Ok(new {name = "Ali"});
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using WebApplication1.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using WebApplication1.Option;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<DataContext>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //add swagger to services-----------------------------------------------------------------------
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+            //-----------------------------------------------------------------------------------------------
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+             }
+            else
+            {
+                app.UseHsts();
+            }
+            
+            var swaggerOption = new SwaggerOption();
+            Configuration.GetSection(nameof(SwaggerOption)).Bind(swaggerOption);
+            //this lines for configure swagger---------------------------------------------------------------------------------------------------
+            app.UseSwagger(opt => { opt.RouteTemplate = swaggerOption.JsonRoute;});
+            app.UseSwaggerUI(opt => {opt.SwaggerEndpoint(swaggerOption.UIEndpoint, swaggerOption.Description); });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            //----------------------------------------------------------------------------------------------------------------------------------- 
+                    
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
